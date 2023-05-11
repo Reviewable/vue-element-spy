5e2c213c5c15a9d9ec6ccb7adde9077fd9f6530d
earlAchromatic <68669571+earlAchromatic@users.noreply.github.com>
2023-05-10 22:02:36 -0400

    Add support for dynamic offset and conditional observation

--- src/vue-element-spy.js
+++ src/vue-element-spy.js
@@ -2,21 +2,48 @@ import SpyService from './spy-service';
 
 const spyService = new SpyService();
 
-const vueElementSpy = {
-  inserted(el, {value}) {
-    spyService.setRefreshInterval();
+function isShallowObjectMatch(a, b) {
+  return Object.keys(a).length === Object.keys(b).length &&
+    Object.keys(a).every(i => a[i] === b[i])
+}
 
-    const config = SpyService.parseConfig(value);
+function setup(el, config) {
+  spyService.setRefreshInterval();
 
-    const target = {callback: config.callback, el, active: false, config};
+  const target = {callback: config.callback, el, active: false, config};
 
-    const context = spyService.getContext(config.context);
+  const context = spyService.getContext(config.context);
 
-    const topObserver = new IntersectionObserver(([entry]) => {
-      let activeNew = entry.boundingClientRect.top <= config.offset;
-      if (config.watchExit) activeNew = activeNew && entry.boundingClientRect.top*-1 < el.offsetHeight;
+  const topObserver = new IntersectionObserver(([entry]) => {
+    let activeNew = entry.boundingClientRect.top <= config.offset;
+    if (config.watchExit) activeNew = activeNew && entry.boundingClientRect.top * -1 < el.offsetHeight;
 
+    if (target.active === activeNew) return;
+    if (config.context) {
+      spyService.handleActivation(context);
+      return;
+    }
+
+    target.active = activeNew;
+    target.callback(activeNew, el);
+  }, {threshold: 1, rootMargin: `${config.offset}px 0px 0px`});
+
+  const anchor = document.createElement('div');
+  anchor.style.cssText = 'width: 0; height: 0; display: block;';
+  anchor.className = 'vue-element-spy-anchor';
+  el.parentElement.insertBefore(anchor, el);
+  target.anchor = anchor;
+
+  topObserver.observe(anchor);
+  target.topObserver = topObserver;
+
+  if (config.watchExit) {
+    const bottomObserver = new IntersectionObserver(([entry]) => {
+      if (entry.boundingClientRect.top > config.offset) return;
+
+      const activeNew = entry.intersectionRatio > 0;
       if (target.active === activeNew) return;
+
       if (config.context) {
         spyService.handleActivation(context);
         return;
@@ -24,51 +51,41 @@ const vueElementSpy = {
 
       target.active = activeNew;
       target.callback(activeNew, el);
-    }, {threshold: 1, rootMargin: `${config.offset}px 0px 0px`});
 
-    const anchor = document.createElement('div');
-    anchor.style.cssText = 'width: 0; height: 0; display: block;';
-    anchor.className = 'vue-element-spy-anchor';
-    el.parentElement.insertBefore(anchor, el);
-    target.anchor = anchor;
+    }, {threshold: 0, rootMargin: `${config.offset}px 0px 0px`});
+    bottomObserver.observe(el);
 
-    topObserver.observe(anchor);
-    target.topObserver = topObserver;
+    target.bottomObserver = bottomObserver;
+  }
+  context.push(target);
+}
 
-    if (config.watchExit) {
-      const bottomObserver = new IntersectionObserver(([entry]) => {
-        if (entry.boundingClientRect.top > config.offset) return;
+function teardown(el) {
+  const {contextKey, target} = spyService.findContextAndTarget(el);
+  if (!target) return;
 
-        const activeNew = entry.intersectionRatio > 0;
-        if (target.active === activeNew) return;
+  target.topObserver.disconnect();
+  if (target.bottomObserver) target.bottomObserver.disconnect();
+  target.anchor.remove();
 
-        if (config.context) {
-          spyService.handleActivation(context);
-          return;
-        }
+  spyService.removeTarget(contextKey, target);
 
-        target.active = activeNew;
-        target.callback(activeNew, el);
+  spyService.resetRefreshInterval();
+}
 
-      }, {threshold: 0, rootMargin: `${config.offset}px 0px 0px`});
-      bottomObserver.observe(el);
-
-      target.bottomObserver = bottomObserver;
-    }
-    context.push(target);
+const vueElementSpy = {
+  inserted(el, {value}) {
+    const config = SpyService.parseConfig(value);
+    if (config.shouldSpy) setup(el, config);
   },
-
+  update(el, {value, oldValue}) {
+    if (isShallowObjectMatch(value, oldValue)) return
+    const config = SpyService.parseConfig(value);
+    teardown(el);
+    if (config.shouldSpy) setup(el, config);
+  },
   unbind(el) {
-    const {contextKey, target} = spyService.findContextAndTarget(el);
-    if (!target) return;
-
-    target.topObserver.disconnect();
-    if (target.bottomObserver) target.bottomObserver.disconnect();
-    target.anchor.remove();
-
-    spyService.removeTarget(contextKey, target);
-
-    spyService.resetRefreshInterval();
+    teardown(el);
   }
 }
 
